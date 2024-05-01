e7f5ed9aedcefa97bd14764e86c7ce34880df25b
Adam Mwaniki <adamndegwa@protonmail.com>
2024-05-01 16:56:46 +0300

    Completes the lab by creating an App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+export default function App (){
+
+    const [randomImage, setRandomImage] = useState([]); // we can also set this to null
+    const [isLoading, setIsLoading] = useState(true); //initializing the loading as true since it will happen before the fetch API
+
+    useEffect(() => {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => {
+                setRandomImage(data.message);
+                setIsLoading(false);// changing the state of the loading to false once the API has fetched the data
+            })
+            .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+    return (
+        <>
+           {isLoading ? (<p>Loading...</p>) : (<img src={randomImage} alt="A Random Dog" />)}
+        </>
+    );
+}
